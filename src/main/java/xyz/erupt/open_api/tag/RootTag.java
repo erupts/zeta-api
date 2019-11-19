1aa57b0bd38965607d3dd3fd5d6ed3ce636b3fb6
122339792@qq.com <122339792@qq.com>
2019-11-19 15:31:27 +0800

    init

---
+++ src/main/java/xyz/erupt/open_api/tag/RootTag.java
@@ -0,0 +1,8 @@
+package xyz.erupt.open_api.tag;
+
+/**
+ * Created by liyuepeng on 2019-08-15.
+ */
+public class RootTag {
+    public static final String HANDLER = "handler";
+}
---
+++ src/main/java/xyz/erupt/open_api/tag/SqlTag.java
@@ -0,0 +1,11 @@
+package xyz.erupt.open_api.tag;
+
+/**
+ * Created by liyuepeng on 2019-11-19.
+ */
+public class SqlTag {
+
+    //缓存方式
+    public static final String CACHE_WAY = "cacheWay";
+
+}
